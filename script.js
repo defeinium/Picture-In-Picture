00464b415cc8145221a8e365dd8acfba6862539d
defeinium <58759273+defeinium@users.noreply.github.com>
2020-09-23 16:09:23 +0800

    Adding script file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button')
 
-//Prompt the user to select a media stream, pass to video element, then play
+//Prompt the user to select a media stream, pass to video element, then play.
 async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia(); /* API */ 
